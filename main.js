22677b4e61a7bada4a324cb5030edd0d089f5564
𝐀𝐛𝐡𝐢𝐬𝐡𝐞𝐤 𝐏𝐚𝐭𝐞𝐥 <abhishekpatel4220@gmail.com>
2024-09-27 12:48:35 +0000

    add gredient

---
+++ main.js
@@ -0,0 +1,41 @@
+const display = document.querySelector("#display");
+const buttonEle = document.querySelectorAll(".buttons button");
+
+// Add click event listeners to buttons
+buttonEle.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (item.id === "clear") {
+      display.innerText = "";
+    } else if (item.id === "backspace") {
+      display.innerText = display.innerText.slice(0, -1);
+    } else if (item.id === "equal") {
+      if (display.innerText === "") {
+        display.innerText = "Empty!";
+        setTimeout(() => (display.innerText = ""), 2000);
+      } else {
+        try {
+          display.innerText = evaluateExpression(display.innerText);
+        } catch (error) {
+          display.innerText = "Error!";
+          setTimeout(() => (display.innerText = ""), 2000);
+        }
+      }
+    } else {
+      display.innerText += item.id;
+    }
+  });
+});
+
+// A safe function to evaluate mathematical expressions
+function evaluateExpression(expr) {
+  return Function('"use strict"; return (' + expr + ")")(); // Safer than eval
+}
+
+// Toggle theme button
+const themeToggleBtn = document.querySelector(".theme-toggler");
+const calculator = document.querySelector(".calculator");
+
+themeToggleBtn.addEventListener("click", () => {
+  calculator.classList.toggle("dark");
+  themeToggleBtn.classList.toggle("active");
+});
